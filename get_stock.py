d80c4592ecc8be521e07a8cae1f4a98626189ce5
yamatowestern <yamatowestern@github.com>
2020-03-05 14:57:19 +0900

    add function to get stock data

--- get_stock.py
+++ get_stock.py
@@ -12,15 +12,23 @@ def get_googlefinance(name):
         print("Error:", sys.exc_info()[0])
         print("Description:", sys.exc_info()[1])
 
-def get_yahoo(name):
+def get_yahoo(name, from_year, to_year):
     stock = pdr.get_data_yahoo(name+'.BK', 
-                            start=datetime.datetime(2017, 10, 1), 
-                            end=datetime.datetime(2017, 12, 30))
+                            start=datetime.datetime(from_year,1,1), 
+                            end=datetime.datetime(to_year,12,31))
     return stock
 
+def load_stock(name):
+    pass
+
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser = argparse.ArgumentParser(description='Process the stock data')
     parser.add_argument('input', type=str,
                         help='given stock name as the input for query')
+    parser.add_argument('from_year', type=int,
+                        help='retrieve data from year')
+    parser.add_argument('to_year', type=int,
+                        help='retrieve data to year')
     args = parser.parse_args()
-    print(get_yahoo(args.input))
+    stock = get_yahoo(args.input, args.from_year, args.to_year)
+    stock.to_csv(args.input + '-' + str(args.from_year) + '-' + str(args.to_year) + '.csv')
